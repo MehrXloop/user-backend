ba506cad5bcfa293981c9271388cd0a070419041
MehrXloop <mehrunissa.pirwani@xloopdigital.com>
2023-06-01 16:33:41 +0500

    refactoring

--- src/main/java/com/glc/user/UserController.java
+++ src/main/java/com/glc/user/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -42,5 +43,8 @@ public void deleteUser(@PathVariable int id){
     userRepository.deleteById(id);
 }
 
-
+@PutMapping("")
+public void updateUser(@RequestBody User user){
+    userRepository.save(user);
+}
 }
